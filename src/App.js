cd4d432a9a9844b0cf84aae549cb447c53811bde
krish-vijayan <krishvijayan55@gmail.com>
2022-08-10 17:37:45 -0400

    Added scroll motion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Description from "./components/Description";
+import {
+  ScrollContainer,
+  ScrollPage,
+  Animator,
+  Sticky,
+  batch,
+  Fade,
+  Zoom,
+  StickyIn,
+  FadeIn,
+  ZoomIn,
+  ZoomOut,
+  Move,
+} from "react-scroll-motion";
 
+const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+const FadeUp = batch(Fade(), Sticky(), Move());
+
+const haveFun = {
+  fontSize: "20vw",
+  textAlign: "center",
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ScrollContainer>
+        <ScrollPage page={0}>
+          <Animator animation={batch(Sticky(55, 40), Fade(), Zoom())}>
+            <Description />
+          </Animator>
+        </ScrollPage>
+
+        <ScrollPage page={1}>
+          <Animator animation={ZoomInScrollOut}>
+            <h1 style={haveFun}>HAVE FUN!</h1>
+          </Animator>
+        </ScrollPage>
+
+        <ScrollPage page={2}>
+          <Animator animation={FadeUp}>
+            <h1>This is where the interative array would go.</h1>
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer>
+    </>
   );
 }
 
---
+++ src/components/Description.js
@@ -0,0 +1,20 @@
+import react from "react";
+import "./Description.css";
+import { ScrollContainer, ScrollPage } from "react-scroll-motion";
+function Description() {
+  return (
+    <>
+      <div>
+        <h1 className="title">Arrays</h1>
+        <p className="paragraph">
+          According to the BBC, an array is defined as a data structure that
+          holds similar, related data. With that in mind, this webpage is meant
+          for you to conceptionalize the idea of an array even better by using
+          an interative visaulization method.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Description;
